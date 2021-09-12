d9fa3890e8f4e44d1580ab2f74df39f79bd96f97
PastryChew <80216822+PastryChew@users.noreply.github.com>
2021-09-12 20:53:52 +0300

    bb

---
+++ hm_8/hm8_Runners/Assets/RelayRaceScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelayRaceScript : MonoBehaviour
+{
+
+    public Transform[] runners = new Transform[5];
+    private int runnerNum = 0;
+    private float passDistance;
+    public Vector3 target;
+    
+    public int speed = 200;
+    public bool canMove = true;
+
+    
+
+
+
+    void Start()
+    {
+       
+        target = runners[runnerNum + 1].position;
+    }
+
+    
+    void Update()
+    {
+        passDistance = Vector3.Distance(runners[runnerNum].position, target);
+        
+        if (canMove)
+        {
+            Run();
+        }
+    }
+
+    public void Run()
+    {
+      
+        
+        runners[runnerNum].position = Vector3.MoveTowards(runners[runnerNum].position, target, Time.deltaTime * speed);
+        runners[runnerNum].LookAt(target);
+        
+
+        
+        if (passDistance <= 0)
+        {
+            runnerNum++;
+
+            if (runnerNum == 5)
+            {
+                runnerNum = 0;
+            }
+
+            if (runnerNum == 4)
+            {
+                target = runners[0].position;
+            }
+            else
+            {
+            target = runners[runnerNum + 1].position;
+            }
+            
+        }
+
+    }
+   
+}
---
+++ hm_8/hm8_Runners/Assets/RunnerScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerScript : MonoBehaviour
+{
+    
+    public Vector3[] points = new Vector3[4];
+
+    public GameObject point1; 
+    public GameObject point2; 
+    public GameObject point3; 
+    public GameObject point4;
+
+    private Vector3 target;
+
+    public float speed;
+
+    public bool forward;
+
+    private int massNum = 0;
+
+
+   
+    void Start()
+    {
+        points[0] = point1.transform.position;
+        points[1] = point2.transform.position;
+        points[2] = point3.transform.position;
+        points[3] = point4.transform.position;
+        target = points[massNum];
+    }
+
+    
+    void FixedUpdate()
+    {
+
+        if (forward)
+        {
+            ForwardMove();
+        }
+        else if (!forward)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+            BackMove();
+        }
+        
+        
+    }
+
+    public void ForwardMove()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        if (transform.position == target)
+        {
+            if (massNum == 3)
+            {
+                forward = false;
+            }
+            else
+            {
+            target = points[massNum++];
+            Debug.Log(massNum);
+            }  
+        }
+    }
+    public void BackMove()
+    {
+        if (transform.position == target)
+        {
+            target = points[massNum--];
+            
+            if (massNum == 0)
+            {
+                forward = true;
+            }
+        }
+
+       
+    }
+}
